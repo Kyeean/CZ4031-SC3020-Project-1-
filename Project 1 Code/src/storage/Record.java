3092f010a4d41a62f70d20224152888bfa60a482
Chan Zhao Yi <zzzhaoyi@gmail.com>
2024-03-06 16:19:35 +0800

    Add ToString to Record and implement NonLeafNode

--- Project 1 Code/src/storage/Record.java
+++ Project 1 Code/src/storage/Record.java
@@ -11,19 +11,32 @@ public class Record {
         this.numVote = numVote;
     }
 
+    // Return Tconst of the record
     public String getTconst() {
         return tConst;
     }
 
+    // Return average rating of the record
     public float getAvgRating() {
         return avgRate;
     }
 
+    // Return number of votes of the record
     public int getNumVotes() {
         return numVote;
     }
 
+    /* Return the size of record (It is fixed),
+    tConst = 2 Bytes * 10 chars = 20 Bytes
+    avgRate = 4 Bytes
+    numVote = 4 Bytes
+    Total = 28 Bytes 
+    */
     public static int size() {
-        return 17;
+        return 28;
+    }
+
+    public String toString() {
+        return String.format("Record Info: Tconst: %s, avgRating: %f, numVotes: %d ", this.getTconst(), this.getAvgRating(), this.getNumVotes());
     }
 }
